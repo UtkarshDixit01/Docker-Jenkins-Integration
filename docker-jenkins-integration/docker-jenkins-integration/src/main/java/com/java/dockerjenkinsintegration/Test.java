ae20f610006305338d0e31640c68faa560f26c9c
utkarshdixit <utkarsh.dixit@nagarro.com>
2023-01-07 16:20:06 +0530

    Second Commit

---
+++ docker-jenkins-integration/docker-jenkins-integration/src/main/java/com/java/dockerjenkinsintegration/Test.java
@@ -0,0 +1,11 @@
+package com.java.dockerjenkinsintegration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+
+	}
+
+}
